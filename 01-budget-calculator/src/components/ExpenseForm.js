c5fe187c10d0eb1c958618e80d5172ef56c509bf
alisherfw <57340324+alisherfw@users.noreply.github.com>
2021-09-22 03:19:28 +0900

    form

--- 01-budget-calculator/src/components/ExpenseForm.js
+++ 01-budget-calculator/src/components/ExpenseForm.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { MdSend } from 'react-icons/md'
 
 export const ExpenseForm = () => {
     return (
-        <div>
-            ExpensForm
-        </div>
+        <form>
+            <div className="form-center">
+                <div className="form-group">
+                    <label htmlFor="charge">
+                        charge
+                    </label>
+                    <input type="text" className="form-control" id="charge" name="charge" placeholder="e.g. rent"/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="amount">
+                        amount
+                    </label>
+                    <input type="number" className="form-control" id="amount" name="amount" placeholder="e.g. 100" />
+                </div>
+            </div>
+            <button type="submit" className="btn">
+                submit
+                <MdSend className="btn-icon" />
+            </button>
+        </form>
     )
 }
