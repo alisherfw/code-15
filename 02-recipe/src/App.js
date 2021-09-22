fee1a965c8b74f180bb8d60dcfc8d8a4e5c9dc51
alisherfw <57340324+alisherfw@users.noreply.github.com>
2021-09-23 02:47:45 +0900

    pblsh

--- 02-recipe/src/App.js
+++ 02-recipe/src/App.js
@@ -7,29 +7,52 @@ export const App = () => {
   const APP_ID = "bfca593f";
   const APP_KEY = "c9fbd3bf5365992ab5e2850bc396bbbd";
 
-  const [recipes, setRecipes] = useState([])
+  
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState("Fish");
 
   useEffect(() => {
     getRecipes();
-  }, [])
+  }, [query])
+
+  
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
-    console.log(data.hits);
+
+  }
+  const updateSearch = e => {
+    setSearch(e.target.value);
+
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
   }
 
   return (
     <div className="App">
-      <form className="searchForm">
-        <input className="searchBar" type="text" />
-        <button className="searchButton" type="submit">Search</button>
+      <form onSubmit={getSearch} className="searchForm">
+        <input className="searchBar" type="text" value={search} onChange={updateSearch} />
+        <button  className="searchButton" type="submit">Search</button>
         
       </form>
+      <div className="recipes">
       {recipes.map(recipe => (
-          <Recipe />
+          <Recipe 
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
         ))}
+      </div>
+      
     </div>
   )
   
--- 02-recipe/src/Recipe.js
+++ 02-recipe/src/Recipe.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import style from './recipe.module.css'
 
-const Recipe = () => {
+const Recipe = ({title, calories, image, ingredients}) => {
     return (
-        <div>
-            <h1>Title</h1>
-            <p>Calories</p>
-            <img src="" alt="" />
+        <div className={style.recipe}>
+            <div className={style.title}>
+            <img className={style.image} src={image} alt="" />
+            <h1 >{title}</h1>
+            </div>
+            <p>{calories} calories</p>
+            <ol>
+                {ingredients.map(ingredient => (
+                    <li>{ingredient.text}</li>
+                ))}
+            </ol>
+            
         </div>
     )
 };
